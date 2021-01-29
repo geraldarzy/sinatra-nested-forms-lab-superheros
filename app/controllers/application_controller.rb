bcb89eb2d80e45b22a33d64528c5be95cc4a2767
geraldarzy <geraldarzola@gmail.com>
2021-01-28 23:23:03 -0500

    commit tree c2a38733dcc3c37eb01e310d9da6b959027e1934

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
+
+    post '/teams' do
+        @team = Team.new(params[:team])
+        @hero1 = Superhero.new(params[:team][:heroes][0])
+        @hero2 = Superhero.new(params[:team][:heroes][1])
+        @hero3 = Superhero.new(params[:team][:heroes][2])
+        erb :team
+    end
+
 
 end
