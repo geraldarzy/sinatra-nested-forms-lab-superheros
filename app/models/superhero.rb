9c86c45ce2a52fd444ed231d60db34364040d603
geraldarzy <geraldarzola@gmail.com>
2021-01-28 23:23:11 -0500

    Done.

---
+++ app/models/superhero.rb
@@ -0,0 +1,9 @@
+class Superhero
+    attr_accessor :name, :power, :bio
+
+    def initialize(hash)
+        @name = hash[:name]
+        @power = hash[:power]
+        @bio = hash[:biography]
+    end
+end
